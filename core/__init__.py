05d738bc60158368f9328fe3986366b68081a0fc
Prasun Roy <38404108+prasunroy@users.noreply.github.com>
2019-02-15 01:15:00 +0530

    Package initialization

---
+++ core/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Package initialization.
+Created on Tue Feb 12 22:00:00 2019
+Author: Prasun Roy | CVPRU-ISICAL (http://www.isical.ac.in/~cvpr)
+GitHub: https://github.com/prasunroy/deep-music
+
+"""
+
