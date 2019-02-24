05f159620d9ee886a3b173e908fd9bcf62768fe3
Prasun Roy <38404108+prasunroy@users.noreply.github.com>
2019-02-24 11:22:44 +0530

    :boom: Updated module

--- core/utils.py
+++ core/utils.py
@@ -7,111 +7,83 @@ GitHub: https://github.com/prasunroy/deep-music
 
 """
 
-
 # imports
 import subprocess
 
-
-# extract audio from a video
-def extract_audio(src, dst, start=0, duration=-1, bitrate=192, overwrite=False,
-                  verbose=False):
-    """Extracts audio from a video.
+# download a video from YouTube
+def download_video(ytid, out_dir='.', verbose=False):
+    """Downloads a video from YouTube.
     
     Args:
-        src      : Path to source video file.
-        dst      : Path to target audio file.
-        start    : Start position in seconds. Defaults to 0.
-        duration : Duration of the extracted audio clip. For values <= 0 full
-                   audio clip is extracted. Defaults to -1.
-        bitrate  : Bit rate of target audio in KHz. Possible values are 8, 16,
-                   24, 32, 40, 48, 64, 80, 96, 112, 128, 160, 192, 224, 256,
-                   or 320. Defaults to 192.
-        overwrite: Flag for overwriting target audio file. Defaults to False.
-        verbose  : Flag for verbose mode. Defaults to False.
-    
-    """
-    options = ['ffmpeg']
-    if start >= 0 and duration >= 1:
-        options.append('-ss {} -i {} -t {}'.format(start, src, duration))
-    else:
-        options.append('-i {}'.format(src))
-    options.append('-b:a {}k'.format(bitrate))
-    options.append('-vn')
-    if overwrite:
-        options.append('-y')
-    else:
-        options.append('-n')
-    if verbose:
-        options.append('-v info')
-    else:
-        options.append('-v quiet')
-    options.append(dst)
-    command = ' '.join(options)
-    subprocess.call(command, shell=True)
-    return
-
-
-# remove audio from a video
-def extract_video(src, dst, start=0, duration=-1, overwrite=False,
-                  verbose=False):
-    """Removes audio from a video.
-    
-    Args:
-        src      : Path to source video file.
-        dst      : Path to target video file.
-        start    : Start position in seconds. Defaults to 0.
-        duration : Duration of the extracted video clip. For values <= 0 full
-                   video clip is extracted. Defaults to -1.
-        overwrite: Flag for overwriting target video file. Defaults to False.
-        verbose  : Flag for verbose mode. Defaults to False.
-    
-    """
-    options = ['ffmpeg']
-    if start >= 0 and duration >= 1:
-        options.append('-ss {} -i {} -t {}'.format(start, src, duration))
-    else:
-        options.append('-i {}'.format(src))
-    options.append('-c copy')
-    options.append('-an')
-    if overwrite:
-        options.append('-y')
-    else:
-        options.append('-n')
-    if verbose:
-        options.append('-v info')
-    else:
-        options.append('-v quiet')
-    options.append(dst)
-    command = ' '.join(options)
-    subprocess.call(command, shell=True)
-    return
-
-
-# download video from YouTube
-def download_video(ytid, out_dir=None, out_fmt=None, verbose=False):
-    """Downloads video from YouTube.
-    
-    Args:
-        ytid    : YouTube video identifier.
-        out_dir : Download directory. Uses current working directory if not
-                  specified. Defaults to None.
-        out_fmt : Target video format. Uses FFMPEG to convert downloaded video
-                  into specified format. Skips conversion if not specified.
-                  Defaults to None.
-        verbose : Flag for verbose mode. Defaults to False.
+        ytid   : YouTube video identifier.
+        out_dir: Download directory. Defaults to current working directory.
+        verbose: Execute in verbose mode. Defaults to False.
     
     """
     options = ['youtube-dl']
-    if not out_dir is None:
-        options.append('-o "{}/%(id)s.%(ext)s"'.format(out_dir))
-    else:
-        options.append('-o "%(id)s.%(ext)s"')
-    if not out_fmt is None:
-        options.append('--recode-video {}'.format(out_fmt))
+    options.append('-o "{}/%(id)s.%(ext)s"'.format(out_dir))
     if not verbose:
         options.append('--quiet --no-warnings')
     options.append('https://www.youtube.com/watch?v={}'.format(ytid))
+    command = ' '.join(options)
+    subprocess.call(command, shell=True)
+    return
+
+# recode media
+def recode_media(src, dst, start=0, length=0, width=0, height=0, fps=0,
+                 bitrate_v=0, bitrate_a=0, no_video=False, no_audio=False,
+                 copy_v=False, copy_a=False, overwrite=False, verbose=False):
+    """Recodes media.
     
+    Args:
+        src      : Path to source media file.
+        dst      : Path to target media file.
+        start    : Start position in seconds. Ignored if <= 0
+        length   : Length of recoded media in seconds. Ignored if <= 0.
+        width    : Frame width of recoded video. Ignored if <= 0.
+        height   : Frame height of recoded video. Ignored if <= 0.
+        fps      : Frame rate of recoded video. Ignored if <= 0.
+        bitrate_v: Bit rate of recoded video stream in KHz. Ignored if <= 0.
+        bitrate_a: Bit rate of recoded audio stream in KHz. Ignored if <= 0.
+        no_video : Drop video stream. Defaults to False.
+        no_audio : Drop audio stream. Defaults to False.
+        copy_v   : Copy video stream directly from source. Defaults to False.
+        copy_a   : Copy audio stream directly from source. Defaults to False.
+        overwrite: Overwrite target media file. Defaults to False.
+        verbose  : Execute in verbose mode. Defaults to False.
+    
+    """
+    options = ['ffmpeg']
+    options.append('-i {}'.format(src))
+    if start >= 0:
+        options.append('-ss {}'.format(start))
+    if length > 0:
+        options.append('-t {}'.format(length))
+    if width > 0 and height > 0:
+        options.append('-s {}x{}'.format(width, height))
+    if fps > 0:
+        options.append('-r {}'.format(fps))
+    if bitrate_v > 0:
+        options.append('-b:v {}k'.format(bitrate_v))
+    if bitrate_a > 0:
+        options.append('-b:a {}k'.format(bitrate_a))
+    if copy_v:
+        options.append('-c:v copy')
+    if copy_a:
+        options.append('-c:a copy')
+    if no_video:
+        options.append('-vn')
+    if no_audio:
+        options.append('-an')
+    if overwrite:
+        options.append('-y')
+    else:
+        options.append('-n')
+    if verbose:
+        options.append('-v info')
+    else:
+        options.append('-v quiet')
+    options.append(dst)
     command = ' '.join(options)
     subprocess.call(command, shell=True)
     return
